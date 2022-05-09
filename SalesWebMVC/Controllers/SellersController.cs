1c3a74e9073dda5b47f548dfa8ee1bbeddb18e3f
gabrieldoria2000 <gabrieldoria2000@gmail.com>
2022-05-08 21:07:24 -0300

    ForeignKey not null

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -18,5 +19,21 @@ namespace SalesWebMVC.Controllers
             var List = _sellerService.FindAll();
             return View(List);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        //Esse ANOTATION validate é pra previnir que alguem use codigos maliciosos aproveitando a sua autenticação
+        //e esse ANOTATION httppost é pra indicar que isso é uma ação de post e não de GET
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction("Index");
+
+        }
     }
 }
+
--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -11,6 +11,8 @@ namespace SalesWebMVC.Models
         public DateTime Birthdate { get; set; }
         public double BaseSalary { get; set; }
 
+        public int DepartmentId { get; set; }
+
         public Department Department { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
